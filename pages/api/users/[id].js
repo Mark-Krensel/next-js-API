f6fdf36ebd7afb8ff5da08c9c14d51ca676e4544
Mark Krensel <mark.krensel@gmail.com>
2022-10-08 18:16:59 +0200

    add get, post and delete functionality

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,18 @@
+import {getUserById, deleteUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
+  const {id} = request.query;
+
+  switch (request.method) {
+    case 'GET':
+      const user = await getUserById(id);
+      return response.status(200).json(user);
+    case 'DELETE':
+      deleteUser(id);
+      return response
+        .status(202)
+        .json({message: `user was deleted`, deletedUser: id});
+  }
+
   res.status(403).json({message: 'Error: request method not allowed.'});
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,7 +1,17 @@
+import {getAllUsers, createUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
-  if (request.method === 'GET') {
-    const data = [];
-    return response.status(200).json(data);
+  switch (request.method) {
+    case 'GET':
+      const data = await getAllUsers();
+      return response.status(200).json(data);
+
+    case 'POST':
+      const newUser = JSON.parse(request.body);
+      createUser(newUser.name, newUser.gender, newUser.email);
+      return response
+        .json(201)
+        .json({message: 'User created', newUser: request.body});
   }
 
   res.status(403).json({message: 'Error: request method not allowed.'});
